21af739aaa2bda56d4b638b0afee98987fcae774
jlturner85 <jlturner85@gmail.com>
2012-12-08 02:07:57 -0600

    splash screen and moving backgrounds

---
+++ Assets/Backgrounds.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+namespace GameJamTest.Assets
+{
+    class Backgrounds
+    {
+        public Texture2D texture;
+        public Rectangle rectangle;
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, rectangle, new Color(255,255,255,255));
+        }
+    }
+
+    class Scrolling : Backgrounds
+    {
+        public Scrolling(Texture2D newTexture, Rectangle newRectangle)
+        {
+            texture = newTexture;
+            rectangle = newRectangle;
+        }
+
+        public void Update(int speed)
+        {
+            rectangle.X -= speed;
+        }
+    }
+
+}
